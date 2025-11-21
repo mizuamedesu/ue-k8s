c874b1312425bb40fd7a4ef86d606b51bfc8702b
Ryo Sato <97249122+mizuamedesu@users.noreply.github.com>
2025-11-21 15:57:09 +0900

    fix: tailscale

--- vm/master_setup.sh
+++ vm/master_setup.sh
@@ -52,10 +52,7 @@ timeout: 10
 INNER_EOF
 
 echo "Installing Tailscale..."
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-sudo apt-get update
-sudo apt-get install -y tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
 
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
